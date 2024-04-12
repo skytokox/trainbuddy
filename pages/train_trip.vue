b8820bc7440f049713069f6988a472d3e52a004a
sky <2iamsky5@gmail.com>
2024-04-12 13:54:35 +0200

    mobile

--- pages/train_trip.vue
+++ pages/train_trip.vue
@@ -86,8 +86,8 @@ async function getConnections() {
           <UPopover :popper="{ placement: 'bottom-start' }" >
             <UButton icon="i-heroicons-calendar-days-20-solid" :label="format(date, 'd MMM, yyy')" />
 
-            <template #panel="{ close }">
-              <DatePicker v-model="date" is-required @close="close" />
+            <template>
+              <DatePicker v-model="date" is-required />
             </template>
           </UPopover>
         </div>
