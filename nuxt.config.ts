063f1f82e2d86bc7a2d34401201ef67586331849
sky <2iamsky5@gmail.com>
2024-04-12 09:56:15 +0200

    bruno...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,21 +2,22 @@
 import { resolve } from 'pathe';
 
 export default defineNuxtConfig({
-  runtimeConfig: {
-    auth0: {
-      clientId: 'nPmYz3aF4bChYOTT11JxtAsQFm2NS6EX',
-      clientSecret: '0mjRrh2yqlrK1Sg90dOHkem-rKIffNrjopIoI_AhpAVybqdIl7KywlI8C8p7iq4S',
-      issuer: 'dev-oes55s56ln5dkwf8.eu.auth0.com'
-    }
-  },
+  // runtimeConfig: {
+  //   auth0: {
+  //     clientId: 'nPmYz3aF4bChYOTT11JxtAsQFm2NS6EX',
+  //     clientSecret: '0mjRrh2yqlrK1Sg90dOHkem-rKIffNrjopIoI_AhpAVybqdIl7KywlI8C8p7iq4S',
+  //     issuer: 'dev-oes55s56ln5dkwf8.eu.auth0.com'
+  //   }
+  // },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@vueuse/nuxt', '@sidebase/nuxt-auth'],
-  auth: {
-    provider: {
-      type: 'authjs'
-    }
-  },
+  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@vueuse/nuxt'],
+  //@sidebase/nuxt-auth'
+  // auth: {
+  //   provider: {
+  //     type: 'authjs'
+  //   }
+  // },
   colorMode: {
     preference: 'light',
     classSuffix: ''
