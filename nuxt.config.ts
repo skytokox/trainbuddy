c1d386731005e1afc111a91845e556b51938bdba
sky <2iamsky5@gmail.com>
2024-04-12 09:46:57 +0200

    test 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,21 @@
 import { resolve } from 'pathe';
 
 export default defineNuxtConfig({
+  runtimeConfig: {
+    auth0: {
+      clientId: 'nPmYz3aF4bChYOTT11JxtAsQFm2NS6EX',
+      clientSecret: '0mjRrh2yqlrK1Sg90dOHkem-rKIffNrjopIoI_AhpAVybqdIl7KywlI8C8p7iq4S',
+      issuer: 'dev-oes55s56ln5dkwf8.eu.auth0.com'
+    }
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@vueuse/nuxt',],
+  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@vueuse/nuxt', '@sidebase/nuxt-auth'],
+  auth: {
+    provider: {
+      type: 'authjs'
+    }
+  },
   colorMode: {
     preference: 'light',
     classSuffix: ''
@@ -19,5 +31,10 @@ export default defineNuxtConfig({
     options: {
       scrollBehaviorType: 'smooth'
     }
-  }
+  },
+  routeRules: {
+    '/api/**': {
+      cors: true,
+    },
+  },
 })
